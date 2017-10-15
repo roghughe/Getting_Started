8e3cf72ecd341910c4b809790935503572a81de0
Roger Hughes <info@captaindebug.com>
2017-10-15 20:02:37 +0100

    cookie sample completed

--- cookiesample/cookiesample.go
+++ cookiesample/cookiesample.go
@@ -12,9 +12,16 @@ import (
 	"net/http"
 	"errors"
 )
+ const (
+ 	cookieName = "test"
+ 	header = "Set-Cookie"
+ )
 
-var key = []byte("the-key-has-to-be-32-bytes-long!")
+var (
+	key = []byte("the-key-has-to-be-32-bytes-long!")
 
+
+)
 
 
 // This is an example of how to encrypt a struct and attach it to a cookie
@@ -25,7 +32,7 @@ type UserRights struct {
 	// define the user's roles
 	admin    bool		// These are the roles - this user has admin rights
 	edit     bool       // The user has edit rights
-	tracking bool
+	user     bool		// This is an ordinary user
 }
 
 
@@ -44,26 +51,32 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		// Get the user's rights
 		rights := getRights(username)
 		cookieValue, err := encrypt(rights)
-		if err == nil {
+		if err != nil {
 			// okay handle this - redirect the user to an error page
+			fmt.Fprintf(w, "Error creating the cookie, %s ",err.Error())
+			return
 		}
 
 		// Create the cookie
 		maxAge := 8 * 60 * 60 // expires after 8 hours
 		cookie := http.Cookie{
-			Name:  "test",
+			Name:  cookieName,
 			Value: base64.StdEncoding.EncodeToString(cookieValue),
 			MaxAge: maxAge,
 			Secure: true,
 		}
 
 		// Store the cookie in the response to send it back to the browser
-		v := cookie.String()
-		w.Header().Add("Set-Cookie", v)
+		http.SetCookie(w, &cookie)
+		fmt.Fprintf(w, "Login Okay")
+		return
 	}
+
+	fmt.Fprintf(w, "Invalid User")
 }
 
 // Validation is beyond the scope to this sample
+// Always validate any user
 func validateUser(username, password string) bool {
 	return true
 }
@@ -77,7 +90,7 @@ func getRights(username string) *UserRights {
 		username: username,
 		admin: true,
 		edit:  true,
-		tracking: true,
+		user: true,
 	}
 }
 
@@ -109,41 +122,75 @@ func encrypt(rights *UserRights) ([]byte, error) {
 }
 
 
-// This is any old handler func - this does anything, but not before decrypting the cookie
-func someHandlerFunc(w http.ResponseWriter, r *http.Request) {
+// This is a handler func - decide what to server the user based on roles
+func SomeHandlerFunc(w http.ResponseWriter, r *http.Request) {
 
-	if ok, err := validateRequest(&w,r); err != nil {
-
-		if err != nil {
-			// display an error - this is an invalid cookie
-		}
-
-		if !ok {
-			// At this point display 'invalid user page'
-		}
+	userRights, err := validateRequest(&w,r)
+	if err == http.ErrNoCookie {
+		// Display the login page - the cookie doesn't exist
+		fmt.Fprintf(w, "Please Login")
+	} else if err  != nil {
+		fmt.Fprintf(w, "Error %s",err.Error())
+		// display an error - this is an invalid cookie
 	}
-
 
 	// OKAY at this point diplay the page + data the user's after
-
-
-}
-
-func validateRequest(w * http.ResponseWriter, r *http.Request) (bool, error) {
-
-
-	cookie, err := r.Cookie("test")
-	if err != nil {
-		fmt.Printf("Cookie error: %+v\n", err.Error())
-		// Okay, so return an error page
+	// Check the rights and display whatever data is necessary eg:
+	if userRights.admin {
+		// This is an admin user display stuff
+		fmt.Fprintf(w, "Hi admin user")
+	} else if userRights.edit {
+		// This user has edit rights
+		fmt.Fprintf(w, "Hi edit user")
+	} else if userRights.user {
+		// This user is a normal user
+		fmt.Fprintf(w, "Hi ordinary user")
+	} else {
+		// This is an invalid user struct - display an error
 	}
-	return true, nil
+}
+
+// This is any old handler func - server all users the same thing
+func SomeOtherHandlerFunc(w http.ResponseWriter, r *http.Request) {
+
+	_, err := validateRequest(&w,r)
+	if err == http.ErrNoCookie {
+		// Display the login page - the cookie doesn't exist
+		fmt.Fprintf(w, "Please login")
+		return
+	} else if err  != nil {
+		// display an error - this is an invalid cookie
+		fmt.Fprintf(w, "Error... %s",err.Error())
+		return
+	}
+
+	// This is an admin user display stuff
+	fmt.Fprintf(w, "Hi user")
 }
 
 
+// Validate the cookie. Is the user logged in? What roles does he/she have?
+// This is a key function - apply this to all handlers
+func validateRequest(w * http.ResponseWriter, r *http.Request) (*UserRights, error) {
+
+	cookie, err := r.Cookie(cookieName)
+	if err  != nil {
+		return nil, err
+	}
+
+	encoded, err := base64.StdEncoding.DecodeString(cookie.Value)
+	if err != nil {
+		// Cannot decode the cookie - ensure that the handler displays the error page
+		return nil, err
+	}
+
+	rights, err := decrypt(encoded,key)
+	return rights, nil
+}
 
 
-func decrypt(ciphertext []byte, key []byte) ([]byte, error) {
+// Decrypt the bytes, converting them back to to a struct
+func decrypt(ciphertext []byte, key []byte) (*UserRights, error) {
 	c, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -160,7 +207,19 @@ func decrypt(ciphertext []byte, key []byte) ([]byte, error) {
 	}
 
 	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
-	return gcm.Open(nil, nonce, ciphertext, nil)
+	var decoded []byte
+	decoded, err = gcm.Open(nil, nonce, ciphertext, nil)
+
+	var userRights UserRights
+	err = json.Unmarshal(decoded,&userRights)
+
+	if err != nil {
+		// This should lead to the display of an error page
+		fmt.Printf("Error: %s", err)
+		return nil, err;
+	}
+
+	return &userRights,nil
 }
 
 
--- cookiesample/cookiesample_test.go
+++ cookiesample/cookiesample_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 	"fmt"
 	"net/http/httptest"
+	"net/http"
+	"bufio"
+	"strings"
 )
 
 func TestLogin(t *testing.T) {
@@ -16,7 +19,63 @@ func TestLogin(t *testing.T) {
 
 	headerMap := w.HeaderMap
 	cookie := headerMap.Get("Set-Cookie")
-
 	fmt.Printf("Cookie is: %+v\n",cookie)
+	result := string(w.Body.Bytes())
+	if result != "Login Okay" {
+		t.Fatalf("Invalid body: %s\n",result)
+	}
+
 }
 
+
+
+func TestSomeOtherHandlerFunc(t *testing.T) {
+
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	w := httptest.NewRecorder()
+
+	Login(w, req) // The user must be logged in
+
+	// Copy the Cookie over to a new Request
+	// The browser will do this bit before it makes the next requrest
+	req = httptest.NewRequest("GET", "http://example.com/foo", nil)
+	rawCookies := w.Header().Get("Set-Cookie")
+	rawRequest := fmt.Sprintf("GET / HTTP/1.0\r\nCookie: %s\r\n\r\n", rawCookies)
+
+	req, err := http.ReadRequest(bufio.NewReader(strings.NewReader(rawRequest)))
+	var cookie *http.Cookie
+	if err == nil {
+		cookie, err = req.Cookie(cookieName)
+		req.AddCookie(cookie)
+
+		// Now ensure that we get the page we want
+		SomeOtherHandlerFunc(w,req)
+
+		body := w.Body
+		result := string(body.Bytes())
+		if result != "Login OkayHi user" {
+			t.Fatalf("Invalid body: %s\n",result)
+		}
+		return
+	}
+
+	t.Error("Can't copy cookie to the new request")
+}
+
+
+func TestSomeOtherHandlerFunc_user_not_logged_in(t *testing.T) {
+
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	w := httptest.NewRecorder()
+
+	// Login not called
+	SomeOtherHandlerFunc(w,req)
+
+	body := w.Body
+
+	result := string(body.Bytes())
+
+	if result != "Please login" {
+		t.Fatalf("Invalid body: %s\n",result)
+	}
+}
