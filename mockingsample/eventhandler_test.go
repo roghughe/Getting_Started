d545cc2d23b112d9dc9176072a07097db040e748
Roger Hughes <info@captaindebug.com>
2017-10-22 16:29:03 +0100

    Mocking in Golang - sample

--- mockingsample/eventhandler_test.go
+++ mockingsample/eventhandler_test.go
@@ -6,19 +6,19 @@ import (
 	"testing"
 )
 
-// A DummyConnection mimics a real database connection - but allows us to mock the connection and follow happy and fail code paths
-type DummyConnection struct {
+// A MockConnection mimics a real database connection - but allows us to mock the connection and follow happy and fail code paths
+type MockConnection struct {
 
 	fail bool // Set this to true to mimic a DB read / write failure
 }
 
-var dummyError = errors.New("This is my test error")
+var dummyError = errors.New("this is my test error")
 
 /* This section contains the mock database access functions */
 
 // This is the mock database read function
-func (r *DummyConnection) ReadSomething(arg0, arg1 string) ([]string, error)  {
-	fmt.Printf("This is the MOCK database driver - read args: %s -- %s",arg0,arg1)
+func (r *MockConnection) ReadSomething(arg0, arg1 string) ([]string, error)  {
+	fmt.Printf("This is the MOCK database driver - read args: %s -- %s\n",arg0,arg1)
 
 	if r.fail {
 		fmt.Println("Whoops - there's been a database write error")
@@ -29,8 +29,8 @@ func (r *DummyConnection) ReadSomething(arg0, arg1 string) ([]string, error)  {
 }
 
 // This is mock database write function
-func (r * DummyConnection) WriteSomething(arg0 []string) error {
-	fmt.Printf("This is the MOCK database driver - write args: %v",arg0)
+func (r * MockConnection) WriteSomething(arg0 []string) error {
+	fmt.Printf("This is the MOCK database driver - write args: %v\n",arg0)
 
 	if r.fail {
 		fmt.Println("Whoops - there's been a database write error")
@@ -46,7 +46,7 @@ func (r * DummyConnection) WriteSomething(arg0 []string) error {
 // Test calling the event handler with a dummy database connection.
 func TestEventHandlerDB_happy_flow(t *testing.T) {
 
-	testCon := DummyConnection{
+	testCon := MockConnection{
 		fail: false,
 	}
 
@@ -64,7 +64,7 @@ func TestEventHandlerDB_happy_flow(t *testing.T) {
 // Test calling the event handler with a dummy database connection, for the failure flow.
 func TestEventHandlerDB_fail_flow(t *testing.T) {
 
-	testCon := DummyConnection{
+	testCon := MockConnection{
 		fail: true,
 	}
 
